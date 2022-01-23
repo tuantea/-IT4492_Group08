73f85002923ffeb58bd96846a25e36502525623c
tuantea <tuan1722000@gmail.com>
2022-01-23 17:04:00 +0700

    update

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const adminController = require('../controller/admin')
 
 router.get('/phone',adminController.getUserByPhone)
 router.get('/all',adminController.getAllUser)
-router.delete('/deleteuser',adminController.getUserById)
+router.get('/userbyid',adminController.getUserById)
 router.get('/active',adminController.active)
 router.get('/inactive',adminController.inactive)
 router.get('/blocked',adminController.blocked)
